c8e75df9004b39cd8fe6cff493233916ac83b5a6
Artur Gaster <gasterartur@googlemail.com>
2024-11-30 14:06:52 +0100

    Create app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import UserList from './components/UserList';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Redux User List</h1>
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
